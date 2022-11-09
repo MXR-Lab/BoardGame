74c23abf4a6a85fb6d2ec30d1c9f2fad3476ed06
Npfant <npfant@gmail.com>
2022-11-08 22:31:17 -0600

    Video clip working?

--- BoardGameV2/Assets/GameManager.cs
+++ BoardGameV2/Assets/GameManager.cs
@@ -2,30 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public GameObject enviroment;
-    public GameObject button1;
+    public GameObject button;
+    public Canvas c1;
+    VideoClipContainer v1;
 
     void Start()
     {
         videoPlayer.loopPointReached += EnableEnviroment;
     }
-    public void PlayVideo(string url)
+    public void PlayVideo(VideoClip vid)
     {
         enviroment.SetActive(false);
-        videoPlayer.url = url;
+        videoPlayer.clip = vid;
+        Debug.Log("Video Playing");
     }
 
     public void videoSelect()
     {
-        button1.SetActive(true);
+        button.SetActive(true);
     }
     public void videoDeselect()
     {
-        button1.SetActive(false);
+        button.SetActive(false);
     }
 
        void EnableEnviroment(VideoPlayer vp)
